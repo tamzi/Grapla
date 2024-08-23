6005b028fdb71bf64ab409603cb5b87280df222b
tamzi <tamrefrank@gmail.com>
2024-08-23 19:14:46 +0200

    initialize build logic and convention

--- app/src/main/java/com/tamzi/grapla/MainActivity.kt
+++ app/src/main/java/com/tamzi/grapla/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             GraplaTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Grapla",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -42,6 +42,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     GraplaTheme {
-        Greeting("Android")
+        Greeting("Grapla")
     }
 }
