db3c43373aa29dcf284f44d8f8a8648a35772584
tamzi <tamrefrank@gmail.com>
2024-08-29 13:16:33 +0200

    Add grapla fix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,8 @@
 plugins {
-    alias(libs.plugins.android.application)
-    alias(libs.plugins.jetbrains.kotlin.android)
-    alias(libs.plugins.compose.compiler)
+    alias(libs.plugins.grapla.android.application)
+    alias(libs.plugins.grapla.android.application.compose)
+    alias(libs.plugins.grapla.hilt)
+    alias(libs.plugins.roborazzi)
 }
 
 android {
@@ -10,8 +11,6 @@ android {
 
     defaultConfig {
         applicationId = "com.tamzi.grapla"
-        minSdk = 28
-        targetSdk = 34
         versionCode = 1
         versionName = "0.0.01" // X.Y.ZZ; X = Major, Y = minor, Z = Patch level can be many digits
 
@@ -20,7 +19,6 @@ android {
             useSupportLibrary = true
         }
     }
-
     buildTypes {
         release {
             isMinifyEnabled = false
@@ -30,18 +28,10 @@ android {
             )
         }
     }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
-    buildFeatures {
-        compose = true
-    }
-    composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.15"
+    testOptions {
+        unitTests {
+            isIncludeAndroidResources = true
+        }
     }
     packaging {
         resources {
@@ -67,4 +57,33 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    implementation(libs.androidx.activity.compose)
+    implementation(libs.androidx.compose.material3.adaptive)
+    implementation(libs.androidx.compose.material3.adaptive.layout)
+    implementation(libs.androidx.compose.material3.adaptive.navigation)
+    implementation(libs.androidx.compose.material3.windowSizeClass)
+    implementation(libs.androidx.compose.runtime.tracing)
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.core.splashscreen)
+    implementation(libs.androidx.hilt.navigation.compose)
+    implementation(libs.androidx.lifecycle.runtimeCompose)
+    implementation(libs.androidx.navigation.compose)
+    implementation(libs.androidx.profileinstaller)
+    implementation(libs.androidx.tracing.ktx)
+    implementation(libs.androidx.window.core)
+    implementation(libs.kotlinx.coroutines.guava)
+    implementation(libs.coil.kt)
+
+    ksp(libs.hilt.compiler)
+
+    debugImplementation(libs.androidx.compose.ui.testManifest)
+
+    kspTest(libs.hilt.compiler)
+
+    androidTestImplementation(kotlin("test"))
+    androidTestImplementation(libs.androidx.test.espresso.core)
+    androidTestImplementation(libs.androidx.navigation.testing)
+    androidTestImplementation(libs.androidx.compose.ui.test)
+    androidTestImplementation(libs.hilt.android.testing)
 }
--- build-logic/convention/src/main/kotlin/AndroidApplicationComposeConventionPlugin.kt
+++ build-logic/convention/src/main/kotlin/AndroidApplicationComposeConventionPlugin.kt
@@ -5,20 +5,19 @@ import org.gradle.api.Project
 import org.gradle.kotlin.dsl.apply
 import org.gradle.kotlin.dsl.getByType
 
-
 /**
  * This plugin is specifically designed to configure Android applications that use Jetpack Compose.
  *
  * This plugin sets up an Android application project with the necessary plugins and configurations
  * to support Jetpack Compose, making it easier to develop modern Android UIs.
- * 
- * The `AndroidApplicationComposeConventionPlugin` class implements 
+ *
+ * The `AndroidApplicationComposeConventionPlugin` class implements
  * the `Plugin<Project>` interface in Gradle, which allows it to be applied to a Gradle project.
- * 
+ *
  * The `apply` method is overridden to define the plugin's behavior when it is applied to a project.
- * Inside this method, the `with(target)` block is used to operate on the `Project` instance 
+ * Inside this method, the `with(target)` block is used to operate on the `Project` instance
  * passed as the `target` parameter.
- * 
+ *
  *  `class AndroidApplicationComposeConventionPlugin : Plugin<Project> {
  *     override fun apply(target: Project) {
  *         with(target) {
@@ -28,17 +27,17 @@ import org.gradle.kotlin.dsl.getByType
  *      }`
  *
  *   Within the `with(target)` block, the `apply` method is called twice to apply
- *   the `com.android.application` and `org.jetbrains.kotlin.plugin.compose` plugins to the project. 
- *   These plugins are essential for building Android applications and enabling 
+ *   the `com.android.application` and `org.jetbrains.kotlin.plugin.compose` plugins to the project.
+ *   These plugins are essential for building Android applications and enabling
  *   Jetpack Compose support, respectively.
  *
  *   `apply(plugin = "com.android.application")
  *    apply(plugin = "org.jetbrains.kotlin.plugin.compose")`
- *      
- *  Next, the `extensions.getByType<ApplicationExtension>()` method is used to retrieve 
- *  the `ApplicationExtension` instance from the project's extensions. 
+ *
+ *  Next, the `extensions.getByType<ApplicationExtension>()` method is used to retrieve
+ *  the `ApplicationExtension` instance from the project's extensions.
  *  This extension provides access to Android-specific configuration options.
- *  
+ *
  *  `val extension = extensions.getByType<ApplicationExtension>()`
  *  
  *  Finally, the `configureAndroidCompose(extension)` function is called with the retrieved
